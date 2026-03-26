1c3b6d5a2909e3faeef64a008778c25da3c3960c
Kerem <kerem.erkart@student.hers.be>
2026-03-26 12:00:42 +0100

    Correction Statut

--- src/main/java/be/hers/pi/comprendre_et_parler/domains/Status.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/domains/Status.java
@@ -1,4 +1,40 @@
 package be.hers.pi.comprendre_et_parler.domains;
 
 public class Status {
+    private String id;
+    private String designation;
+    private int hourQuota;
+
+    /*
+        Constructor of a Status object
+        @param id represent the id
+        @param designation represent the designation
+        @param hourQuota represent the quota of hours
+     */
+    public Status(String id, String designation, int hourQuota) {
+        this.id = id;
+        this.designation = designation;
+        this.hourQuota = hourQuota;
+    }
+
+    /*
+        @return this.id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /*
+        @return this.designation
+     */
+    public String getDesignation() {
+        return designation;
+    }
+
+    /*
+        @return this.hourQuota
+     */
+    public int getHourQuota() {
+        return hourQuota;
+    }
 }
