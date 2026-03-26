cf80b629f52906988bcd50d60b43c22d4cdc4eda
Kerem <kerem.erkart@student.hers.be>
2026-03-26 23:20:52 +0100

    Ajout de nouvelles classes Métier et ajout de leur spec

---
+++ src/main/java/be/hers/pi/comprendre_et_parler/domains/Availability.java
@@ -0,0 +1,5 @@
+package be.hers.pi.comprendre_et_parler.domains;
+
+public class Availability {
+
+}
---
+++ src/main/java/be/hers/pi/comprendre_et_parler/domains/TimeSlot.java
@@ -0,0 +1,45 @@
+package be.hers.pi.comprendre_et_parler.domains;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class TimeSlot {
+    private String id;
+    private LocalTime startHourTime;
+    private LocalTime endHourTime;
+
+    /*
+        Constructor of a TimeSlot object
+        @param id represent the id
+        @param startHourTime represent the hour at which start
+        @param startHourTime represent the hour at which finish
+     */
+    public TimeSlot(String id, LocalTime startHourTime, LocalTime endHourTime) {
+        this.id = id;
+        this.startHourTime = startHourTime;
+        this.endHourTime = endHourTime;
+    }
+
+    /*
+        @return this.id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /*
+        @return this.startHourTime
+     */
+    public LocalTime getStartHourTime() {
+        return startHourTime;
+    }
+
+    /*
+        @return this.startHourTime
+     */
+    public LocalTime getEndHourTime() {
+        return endHourTime;
+    }
+
+
+}
