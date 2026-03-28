4eb6695fe8c1a9e1f07cd92e9c3e6b0fbd5a8835
Kerem <kerem.erkart@student.hers.be>
2026-03-28 11:32:29 +0100

    Complétion classe ExceptionalUnavailability

--- src/main/java/be/hers/pi/comprendre_et_parler/domains/ExceptionalUnavailability.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/domains/ExceptionalUnavailability.java
@@ -3,27 +3,46 @@ package be.hers.pi.comprendre_et_parler.domains;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class ExceptionalUnavailability extends PonctualTimeSlot {
+public class ExceptionalUnavailability  {
+
     private String reason;
+    private PonctualTimeSlot ponctualTimeSlot;
+    private Interpreter interpreter;
 
     /**
-     * Constructor of a PonctualTimeSlot which extends TimeSlot
+     * Constructor of a PonctualTimeSlot w
      *
-     * @param id            represent the id
-     * @param startHourTime represent the hour at which start
-     * @param endHourTime   represent the hour at which finish
-     * @param date          represent the date of the ponctual time slot
      * @param reason        represent the reason of the exceptional unavailability
+     * @param interpreter   represent the interpreter which concern this exceptional unavailability
+     * @param ponctualTimeSlot represent the ponctual time slot which concern this exceptional unavailability
      */
-    public ExceptionalUnavailability(String id, LocalTime startHourTime, LocalTime endHourTime, LocalDate date, String reason) {
-        super(id, startHourTime, endHourTime, date);
+    public ExceptionalUnavailability(String reason, Interpreter interpreter, PonctualTimeSlot ponctualTimeSlot) {
         this.reason = reason;
+        this.interpreter = interpreter;
+        this.ponctualTimeSlot = ponctualTimeSlot;
     }
 
     /**
      * @return this.reason
      */
     public String getReason() {
-        return reason;
+        return this.reason;
     }
+
+    /**
+     * @return this.ponctualTimeSlot
+     */
+    public PonctualTimeSlot getPonctualTimeSlot() {
+        return this.ponctualTimeSlot;
+    }
+
+    /**
+     * @return this.interpreter
+     */
+    public Interpreter getInterpreter(){
+        return this.interpreter
+    }
+
+
+
 }
