e2f2c33cf6fb9cd053c1f9d3295185f4da1b7a44
Kerem <kerem.erkart@student.hers.be>
2026-03-28 11:57:25 +0100

    Nouvelles classes métier ajoutés

---
+++ src/main/java/be/hers/pi/comprendre_et_parler/domains/Beneficiary.java
@@ -0,0 +1,25 @@
+package be.hers.pi.comprendre_et_parler.domains;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Beneficiary extends User {
+
+    /**
+     * Constructor of a Beneficiary extends User
+     *
+     * @param id             represent the id
+     * @param login          represent the login
+     * @param firstName      represent the firstname
+     * @param lastName       represent he lastname
+     * @param birthDate      represent the birthdate
+     * @param hashedPassword represent the hashed password
+     * @param email          represent the email
+     * @param phoneNumber    represent the phone number
+     */
+    public Beneficiary(String id, String login, String firstName, String lastName, LocalDate birthDate, String hashedPassword, String email, String phoneNumber) {
+        super(id, login, firstName, lastName, birthDate, hashedPassword, email, phoneNumber);
+    }
+
+
+}
