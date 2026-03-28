4a58a7740af77889f66ec5302ea3d9f3c6c6dbcb
Kerem <kerem.erkart@student.hers.be>
2026-03-28 22:09:21 +0100

    Correction classe métier

--- src/main/java/be/hers/pi/comprendre_et_parler/domains/Mission.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/domains/Mission.java
@@ -1,20 +1,39 @@
 package be.hers.pi.comprendre_et_parler.domains;
 
 import java.util.List;
+import java.util.Locale;
 
 public class Mission {
+    private int id;
     private String subjet;
     private MissionState stateOfMission;
+    private List<Beneficiary> beneficiaries;
+    private List<Interpreter> interpreters;
+    private Location location;
+    private BusinessSkillCompetence businessSkill;
+    private AcademicSkillCompetence academicSkill;
 
     /**
      * Constructor of a Mission object
      *
-     * @param subjet    represent the subject of the mission
-     * @param stateOfMission  represent the state of the mission
+     * @param id represent the id of the mission
+     * @param subjet represent the subject of the mission
+     * @param stateOfMission represent the state of the mission
+     * @param beneficiaries represent the beneficiaries who concern this mission
+     * @param interpreters represent the interpreters who work for this mission
+     * @param location represent the location of the mission
+     * @param businessSkill represent the required business skill
+     * @param academicSkill represent the required academic skill
      */
-    public Mission(String subjet, MissionState stateOfMission) {
+    public Mission(int id, String subjet, MissionState stateOfMission, List<Beneficiary> beneficiaries, List<Interpreter> interpreters, Location location, BusinessSkillCompetence businessSkill, AcademicSkillCompetence academicSkill) {
+        this.id = id;
         this.subjet = subjet;
         this.stateOfMission = stateOfMission;
+        this.beneficiaries = beneficiaries;
+        this.interpreters = interpreters;
+        this.location = location;
+        this.businessSkill = businessSkill;
+        this.academicSkill = academicSkill;
     }
 
     /**
@@ -25,7 +44,7 @@ public class Mission {
     }
 
     /**
-     * @param subject   represent the subject of Mission
+     * @param subject represent the subject of Mission
      */
     public void setSubject(String subject){
         this.subjet = subject;
@@ -39,7 +58,7 @@ public class Mission {
     }
 
     /**
-     * @param state     represent the mission state
+     * @param state represent the mission state
      */
     public void setStateOfMission(MissionState state){
         this.stateOfMission = state;
@@ -50,5 +69,89 @@ public class Mission {
      */
     public String toString(){
         return null;
+    }
+
+    /**
+     * @return this.id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param newId represent the new id
+     */
+    public void setId(int newId){
+        this.id = newId;
+    }
+
+    /**
+     * @return this.beneficiaries
+     */
+    public List<Beneficiary> getBeneficiaries() {
+        return beneficiaries;
+    }
+
+    /**
+     * @param beneficiaries represent the beneficiaries of the mission
+     */
+    public void setBeneficiaries(List<Beneficiary> beneficiaries) {
+        this.beneficiaries = beneficiaries;
+    }
+
+    /**
+     * @return this.interpreters
+     */
+    public List<Interpreter> getInterpreters() {
+        return interpreters;
+    }
+
+    /**
+     * @param interpreters represent the interpreters of the mission
+     */
+    public void setInterpreters(List<Interpreter> interpreters) {
+        this.interpreters = interpreters;
+    }
+
+    /**
+     * @return this.location
+     */
+    public Location getLocation() {
+        return location;
+    }
+
+    /**
+     * @param location represent the location of the mission
+     */
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    /**
+     * @return this.businessSkill
+     */
+    public BusinessSkillCompetence getBusinessSkill() {
+        return businessSkill;
+    }
+
+    /**
+     * @param businessSkill represent the business skill required for the mission
+     */
+    public void setBusinessSkill(BusinessSkillCompetence businessSkill) {
+        this.businessSkill = businessSkill;
+    }
+
+    /**
+     * @return this.academicSkill
+     */
+    public AcademicSkillCompetence getAcademicSkill() {
+        return academicSkill;
+    }
+
+    /**
+     * @param academicSkill represent the academic skill required for the mission
+     */
+    public void setAcademicSkill(AcademicSkillCompetence academicSkill) {
+        this.academicSkill = academicSkill;
     }
 }
