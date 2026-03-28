101a16036cdb25590f219dc9389ce8b7b265de0c
Kerem <kerem.erkart@student.hers.be>
2026-03-28 21:42:28 +0100

    Ajout nouveau DAO

--- src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAOStatus.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAOStatus.java
@@ -1,4 +1,69 @@
 package be.hers.pi.comprendre_et_parler.DAOs;
 
-public class DAOStatus {
+import be.hers.pi.comprendre_et_parler.domains.Status;
+import be.hers.pi.comprendre_et_parler.exceptions.AlreadyExistsException;
+import be.hers.pi.comprendre_et_parler.exceptions.ConnectionException;
+import be.hers.pi.comprendre_et_parler.exceptions.DuplicatePrimaryKeyException;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class DAOStatus implements DAO<Status>{
+    /**
+     *
+     * @param id the primary key of the object to find in database
+     * @return the object identified by id in database, or null if none was present
+     * @throws ConnectionException if the database could not be reached
+     */
+    @Override
+    public Status find(String id) throws ConnectionException {
+        return null;
+    }
+
+    /**
+     *
+     * @param objectToInsert an object of type T to add to the database
+     * @throws DuplicatePrimaryKeyException if an object matching objectToInsert's id but not all of its attributes is already present in database
+     * @throws AlreadyExistsException       if objectToInsert is already present in database
+     * @throws ConnectionException          if the database could not be reached
+     * @post objectToInsert has been added to the database, and the change was commited
+     */
+    @Override
+    public void create(Status objectToInsert) throws AlreadyExistsException, DuplicatePrimaryKeyException, ConnectionException {
+
+    }
+
+    /**
+     *
+     * @param objectToUpdate the object to edit in the database
+     * @throws NoSuchElementException if no object matching objectToUpdate's id was present in the database
+     * @throws ConnectionException    if the database could not be reached
+     * @post the line referenced by objectToUpdate's id field has been updated with objectToUpdate's attributes, and the change was commited
+     */
+    @Override
+    public void update(Status objectToUpdate) throws AlreadyExistsException, NoSuchElementException, ConnectionException {
+
+    }
+
+    /**
+     *
+     * @param objectToDelete the object to delete in the database
+     * @throws NoSuchElementException if no object matching every attribute of objectToDelete was present in the database
+     * @throws ConnectionException    if the database could not be reached
+     * @post the object matching every attribute of objectToDelete has been deleted from the database, and the change was commited
+     */
+    @Override
+    public void delete(Status objectToDelete) throws NoSuchElementException, ConnectionException {
+
+    }
+
+    /**
+     *
+     * @return every object of the corresponding type present in database (possibly an empty list)
+     * @throws ConnectionException if the database could not be reached
+     */
+    @Override
+    public List<Status> findAll() throws ConnectionException {
+        return List.of();
+    }
 }
