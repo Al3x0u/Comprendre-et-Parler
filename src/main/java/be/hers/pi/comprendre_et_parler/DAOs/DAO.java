bb2a3bc8dee3eefd1a8636cb2a27189c10fbc52b
Luna Lorenz <luna.lorenz@student.hers.be>
2026-03-29 20:12:06 +0200

    Mise à jour de la branche pour suivre la convention de nom des branches d'apres les tickets

--- src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAO.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAO.java
@@ -3,11 +3,21 @@ package be.hers.pi.comprendre_et_parler.DAOs;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.sql.SQLException;
 import be.hers.pi.comprendre_et_parler.exceptions.AlreadyExistsException;
+import be.hers.pi.comprendre_et_parler.exceptions.ConnectionException;
 import be.hers.pi.comprendre_et_parler.exceptions.DuplicatePrimaryKeyException;
 import be.hers.pi.comprendre_et_parler.models.City;
 
 public interface DAO<T> {
+
+    /**
+     *
+     * @param id the primary key of the object to find in database
+     * @return the object identified by id in database, or null if none was present
+     * @throws SQLException if the database could not be reached
+     */
+    T find(String id) throws SQLException;
 
     /**
      *
@@ -17,7 +27,7 @@ public interface DAO<T> {
      * @throws AlreadyExistsException if objectToInsert is already present in database
      * @throws SQLException if the database could not be reached
      */
-    void create(T objectToInsert) throws AlreadyExistsException, DuplicatePrimaryKeyException, SQLException;
+    void create(T objectToInsert) throws AlreadyExistsException, DuplicatePrimaryKeyException, ConnectionException;
 
     /**
      *
@@ -44,3 +54,4 @@ public interface DAO<T> {
      */
     List<T> findAll() throws SQLException;
 }
+
--- src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAOStatus.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAOStatus.java
@@ -2,6 +2,13 @@ package be.hers.pi.comprendre_et_parler.DAOs;
 import be.hers.pi.comprendre_et_parler.models.*;
 import be.hers.pi.comprendre_et_parler.exceptions.*;
 
+import be.hers.pi.comprendre_et_parler.exceptions.AlreadyExistsException;
+import be.hers.pi.comprendre_et_parler.exceptions.DuplicatePrimaryKeyException;
+import be.hers.pi.comprendre_et_parler.model.Status;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
