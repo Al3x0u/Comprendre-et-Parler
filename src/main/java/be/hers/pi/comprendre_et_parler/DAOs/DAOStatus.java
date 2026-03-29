5fde3c713f8b9795caa4b6c7c6440c2c4c375493
Luna Lorenz <luna.lorenz@student.hers.be>
2026-03-29 21:30:38 +0200

    refactor: Connector -> DatabaseConnector

--- src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAOStatus.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAOStatus.java
@@ -24,7 +24,29 @@ public class DAOStatus implements DAO<Status> {
      */
     @Override
     public Status find(String id) throws SQLException {
-        return null;
+        String query = "SELECT * FROM " + table + " WHERE " + field_id + " = ?";
+        PreparedStatement statement = null;
+        ResultSet result = null;
+        Status ret = null;
+        try {
+            statement = DatabaseConnector.getInstance().prepareStatement(query);
+            statement.setInt(1, Integer.parseInt(id));
+            result = statement.executeQuery();
+            if (result.next()) {
+                ret = new Status(
+                        result.getInt(field_id),
+                        result.getString(field_designation),
+                        result.getInt(field_hourQuota)
+                );
+            }
+        }
+        finally {
+            if (statement != null)
+                statement.close();
+            if (result != null)
+                result.close();
+        }
+        return ret;
     }
 
     /**
@@ -35,19 +57,62 @@ public class DAOStatus implements DAO<Status> {
      * @post objectToInsert has been added to the database, and the change was commited
      */
     @Override
-    public void create(Status objectToInsert)
-            throws AlreadyExistsException, DuplicatePrimaryKeyException, SQLException {
+    public void create(Status objectToInsert) throws AlreadyExistsException, DuplicatePrimaryKeyException, SQLException {
+        // Manage invalid states
+        Status alreadyPresent = find(String.valueOf(objectToInsert.getId()));
+        if (alreadyPresent != null) {
+            if (alreadyPresent.equals(objectToInsert))
+                throw new AlreadyExistsException("Object already exists in database");
+            else
+                throw new DuplicatePrimaryKeyException("Object is already present in database under a different primary key");
+        }
+
+        // Attempt insertion
+        String query = "INSERT INTO %s(%s, %s) VALUES(?, ?)";
+        query = String.format(query, table, field_designation, field_hourQuota);
+        PreparedStatement statement = null;
+        try {
+            statement = DatabaseConnector.getInstance().prepareStatement(query);
+            statement.setString(1, objectToInsert.getDesignation());
+            statement.setInt(2, objectToInsert.getHourQuota());
+            statement.executeUpdate();
+        }
+        finally {
+            if (statement != null) {
+                statement.close();
+            }
+        }
     }
 
-    /**
-     * @param objectToUpdate the object to edit in the database
-     * @throws NoSuchElementException if no object matching objectToUpdate's id was present in the database
-     * @throws SQLException if the database could not be reached
-     * @post the line referenced by objectToUpdate's id field has been updated with objectToUpdate's attributes, and the change was commited
-     */
     @Override
-    public void update(Status objectToUpdate)
-            throws AlreadyExistsException, NoSuchElementException, SQLException {
+    public void update(Status objectToUpdate) throws NoSuchElementException, AlreadyExistsException, SQLException {
+        // Manage invalid states
+        List<Status> allLines = findAll();
+        if (allLines.contains(objectToUpdate))
+            return;
+        allLines.forEach((Status line) -> {
+            if (line.getDesignation().equals(objectToUpdate.getDesignation()) && line.getHourQuota() == objectToUpdate.getHourQuota() && line.getId() != objectToUpdate.getId())
+                throw new AlreadyExistsException("Object " + objectToUpdate.getDesignation() + "already exists at id " + line.getId());
+        });
+        if (allLines.stream().anyMatch((Status line) -> line.getId() == objectToUpdate.getId()) == false)
+            throw new NoSuchElementException("Object " + objectToUpdate.getDesignation() + "of id " + objectToUpdate.getId() + "could not be found in database");
+
+        // Attempt update
+        String query = "UPDATE %s SET %s = ?, %s = ? WHERE %s = ?";
+        query = String.format(query, table, field_designation, field_hourQuota, field_id);
+        PreparedStatement statement = null;
+        try {
+            statement = Connector.getInstance().prepareStatement(query);
+            statement.setString(1, objectToUpdate.getDesignation());
+            statement.setInt(2, objectToUpdate.getHourQuota());
+            statement.setInt(3, objectToUpdate.getId());
+            statement.executeUpdate();
+        }
+        finally {
+            if (statement != null) {
+                statement.close();
+            }
+        }
     }
 
     /**
@@ -57,9 +122,26 @@ public class DAOStatus implements DAO<Status> {
      * @post the object matching every attribute of objectToDelete has been deleted from the database, and the change was commited
      */
     @Override
-    public void delete(Status objectToDelete)
-            throws NoSuchElementException, SQLException {
-    }
+    public void delete(Status objectToDelete) throws NoSuchElementException, SQLException {
+            if (find(String.valueOf(objectToDelete.getId())) == null)
+                throw new NoSuchElementException("Object " + objectToDelete.getDesignation() + " was not found in database");
+
+            String query = "DELETE FROM %s WHERE %s = ? AND %s = ? AND %s = ?";
+            query = String.format(query, table, field_id, field_designation, field_hourQuota);
+            PreparedStatement statement = null;
+            try {
+                statement = DatabaseConnector.getInstance().prepareStatement(query);
+                statement.setInt(1, objectToDelete.getId());
+                statement.setString(2, objectToDelete.getDesignation());
+                statement.setInt(3, objectToDelete.getHourQuota());
+                statement.executeUpdate();
+            }
+            finally {
+                if (statement != null) {
+                    statement.close();
+                }
+            }
+        }
 
     /**
      * @return every object of the corresponding type present in database (possibly an empty list)
@@ -67,6 +149,27 @@ public class DAOStatus implements DAO<Status> {
      */
     @Override
     public List<Status> findAll() throws SQLException {
-        return List.of();
+        String query = "SELECT * FROM " + table;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+        List<Status> ret = null;
+        try {
+            statement = DatabaseConnector.getInstance().prepareStatement(query);
+            result = statement.executeQuery();
+            while (result.next()) {
+                ret.add(new Status(
+                        result.getInt(field_id),
+                        result.getString(field_designation),
+                        result.getInt(field_hourQuota)
+                ));
+            }
+        }
+        finally {
+            if (statement != null)
+                statement.close();
+            if (result != null)
+                result.close();
+        }
+        return ret;
     }
 }
--- src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DatabaseConnector.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DatabaseConnector.java
@@ -10,22 +10,22 @@ import java.util.Scanner;
 import java.io.Console;
 
 public class DatabaseConnector {
-    static final String URL = "jdbc:oracle:thin:@labinfo.hers.be:1521:xe";
-    static Connection connection = null;
+    private final static String url = "jdbc:oracle:thin:@labinfo.hers.be:1521:xe";
+    private static Connection connection = null;
 
     private DatabaseConnector() {}
 
     /**
-     * Initialize the connection to the database with the given login and password
+     *
      * @param login the login to the database
      * @param password the password to the database
      * @post a connection to the database has been established
-     * @throws ConnectionException if the connection failed
+     * @throws SQLException if the connection failed
      */
     public static void initialize(String login, String password) throws ConnectionException {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            connection = DriverManager.getConnection(URL, login, password);
+            connection = DriverManager.getConnection(url, login, password);
         }
         catch( ClassNotFoundException e) {
             throw new ConnectionException("Failed to load oracle.jdbc.driver.OracleDriver: " + e);
@@ -36,9 +36,8 @@ public class DatabaseConnector {
     }
 
     /**
-     * Initialize the connection to the database by asking the user for login and password through the console
      * @post the user has been prompted through the console to provide a login and password, and a connection to the database has been established
-     * @throws ConnectionException if the connection failed
+     * @throws SQLException if the connection failed
      */
     public static void initialize() throws ConnectionException {
         Console cons = System.console();
@@ -54,10 +53,10 @@ public class DatabaseConnector {
     }
 
     /**
-     * Return the current connection to the database, and initialize it if necessary
+     *
      * @post A connection to the database has been established. If initialized() hadn't been previously called, the user has been prompted to provide a login and password
      * @return an active Connection to the database
-     * @throws ConnectionException if the connection failed
+     * @throws SQLException if the connection failed
      */
     public static Connection getInstance() throws ConnectionException {
         if (connection == null) {
@@ -66,13 +65,8 @@ public class DatabaseConnector {
         return connection;
     }
 
-    /**
-     * Close the current connection to the database
-     * @post the connection has been closed if it wasn't already
-     * @throws SQLException if a database error occurs
-     */
     public static void closeInstance() throws SQLException {
-        if (connection != null && !connection.isClosed())
+        if (connection != null)
             connection.close();
     }
 }
