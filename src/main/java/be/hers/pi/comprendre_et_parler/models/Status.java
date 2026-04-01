33b02272d7103ca7ce730f9b4518a6a4ecf123d7
Kerem <kerem.erkart@student.hers.be>
2026-04-01 20:04:55 +0200

    Déplacement de Statut de model à models

--- src/main/java/be/hers/pi/comprendre_et_parler/models/Status.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/models/Status.java
@@ -1,53 +1,37 @@
-package be.hers.pi.comprendre_et_parler.models;
+package be.hers.pi.comprendre_et_parler.model;
 
 public class Status {
-    private String id;
+    private int id;
     private String designation;
     private int hourQuota;
 
-    /**
-        Constructor of a Status object
-        @param id represent the id
-        @param designation represent the designation
-        @param hourQuota represent the quota of hours
-     */
-    public Status(String id, String designation, int hourQuota) {
+    public Status(int id, String designation, int hourQuota) {
         this.id = id;
         this.designation = designation;
         this.hourQuota = hourQuota;
     }
 
-    /**
-        @return this.id
-     */
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    /**
-        @return this.designation
-     */
     public String getDesignation() {
         return designation;
     }
 
-    /**
-     * @param designation represent the new designation
-     */
-    public void setDesignation(String designation){
+    public void setDesignation(String designation) {
         this.designation = designation;
     }
-    /**
-        @return this.hourQuota
-     */
+
     public int getHourQuota() {
         return hourQuota;
     }
 
-    /**
-     * @param newQuota represent the new quota
-     */
-    public void setHourQuota(int newQuota){
-        this.hourQuota = newQuota;
+    public void setHourQuota(int hourQuota) {
+        this.hourQuota = hourQuota;
+    }
+
+    public boolean equals(Status other) {
+        return (id == other.id && designation == other.designation && hourQuota == other.hourQuota);
     }
 }
