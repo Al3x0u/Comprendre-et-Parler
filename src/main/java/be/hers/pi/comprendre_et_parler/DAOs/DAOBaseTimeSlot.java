762352a6540a8b3d1001064e1595914f7ab8b416
Luna Lorenz <luna.lorenz@student.hers.be>
2026-04-08 15:20:53 +0200

    feat: DAOBaseTimeSlot.find()

--- src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAOBaseTimeSlot.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAOBaseTimeSlot.java
@@ -4,32 +4,68 @@ import be.hers.pi.comprendre_et_parler.models.BaseTimeSlot;
 import be.hers.pi.comprendre_et_parler.exceptions.AlreadyExistsException;
 import be.hers.pi.comprendre_et_parler.exceptions.DuplicatePrimaryKeyException;
 
-import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DAOBaseTimeSlot implements DAO<BaseTimeSlot> {
 
+    public final String table = "timeslot";
+    public final String field_id = "id";
+    public final String field_startTime = "startHourTime";
+    public final String field_endTime = "endHourTime";
+    public final String field_day = "day";
     /**
-     * Search for a BaseTimeSlot in the database with the int parameter
+     * Search for a BaseTimeSlot in the database with the String parameter
      * @param id the primary key of the object to find in database
      * @return the object identified by id in database, or null if none was present
      * @throws SQLException if the database could not be reached
      */
     @Override
     public BaseTimeSlot find(int id) throws SQLException {
-        return null;
+        String query = "SELECT * FROM " + table + " WHERE " + field_day + " NOT NULL AND " + field_id + " = ?";
+        PreparedStatement statement = null;
+        ResultSet result = null;
+        BaseTimeSlot ret = null;
+        try {
+            statement = DatabaseConnector.getInstance().prepareStatement(query);
+            statement.setInt(1, id);
+            result = statement.executeQuery();
+            if (result.next()) {
+                java.sql.Date startTime = result.getDate(field_startTime);
+                java.sql.Date endTime = result.getDate(field_endTime);
+                ret = new BaseTimeSlot(
+                        result.getInt(field_id),
+                        LocalDateTime.ofEpochSecond(startTime.getTime(), 0, ZoneOffset.ofHours(1)).toLocalTime(), // Hardcoded as GMT+1
+                        LocalDateTime.ofEpochSecond(endTime.getTime(), 0, ZoneOffset.ofHours(1)).toLocalTime(),
+                        startTime.toLocalDate().getDayOfWeek()
+                );
+            }
+        }
+        finally {
+            if (statement != null)
+                statement.close();
+            if (result != null)
+                result.close();
+        }
+        return ret;
     }
 
     /**
      * Insert a BaseTimeSlot object in the database
      * @param objectToInsert an object of type BaseTimeSlot to add to the database
+     * @throws DuplicatePrimaryKeyException if an object matching objectToInsert's id but not all of its attributes is already present in database
      * @throws AlreadyExistsException       if objectToInsert is already present in database
      * @throws SQLException          if the database could not be reached
      * @post objectToInsert has been added to the database, and the change was commited
      */
     @Override
-    public void create(BaseTimeSlot objectToInsert) throws AlreadyExistsException, SQLException {
+    public void create(BaseTimeSlot objectToInsert)
+            throws AlreadyExistsException, DuplicatePrimaryKeyException, SQLException {
 
     }
 
