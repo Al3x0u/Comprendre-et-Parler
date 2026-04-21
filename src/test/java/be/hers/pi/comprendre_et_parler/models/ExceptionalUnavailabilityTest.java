c95c4e6ecc4806911e6e0e1dc85d90108e2a47c1
Allan <aandre@student.hers.be>
2026-04-21 15:19:25 +0200

    fix(ExceptionalUnavailabilityTest): changer l'ordre dans le constructeur qui était incorrect

--- src/test/java/be/hers/pi/comprendre_et_parler/models/ExceptionalUnavailabilityTest.java
+++ src/test/java/be/hers/pi/comprendre_et_parler/models/ExceptionalUnavailabilityTest.java
@@ -17,7 +17,7 @@ class ExceptionalUnavailabilityTest {
     public static void init() {
         p1 = new PunctualTimeSlot(1, LocalTime.NOON, LocalTime.NOON.plusHours(1), LocalDate.now(), LocalDate.now());
         i1 = new Interpreter(20, 30, 1, "1", "test", "test", LocalDate.now(), "1234", "test@gmail.com", "123/45.67.89", new Transportation(1, "test"));
-        e1 = new ExceptionalUnavailability(1, "test", i1, p1);
+        e1 = new ExceptionalUnavailability(1, "test", p1, i1);
     }
 
     @Test
