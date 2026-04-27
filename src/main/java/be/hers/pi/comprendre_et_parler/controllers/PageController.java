6d9235a44d6467450319f8e026e154a37665ca9f
Kerem <kerem.erkart@student.hers.be>
2026-04-27 16:57:12 +0200

    feat : création package controllers

---
+++ src/main/java/be/hers/pi/comprendre_et_parler/controllers/PageController.java
@@ -0,0 +1,9 @@
+package be.hers.pi.comprendre_et_parler.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/horaire")
+public class PageController {
+}
