93b88fa8ccbeb767ea6fdf11aa9e16dcec8a9eb5
Kerem <kerem.erkart@student.hers.be>
2026-04-27 19:12:58 +0200

    fix : retrait controller

--- src/main/java/be/hers/pi/comprendre_et_parler/controllers/PageController.java
+++
@@ -1,9 +0,0 @@
-package be.hers.pi.comprendre_et_parler.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/horaire")
-public class PageController {
-}
