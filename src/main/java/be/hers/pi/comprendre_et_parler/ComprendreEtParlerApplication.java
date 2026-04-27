aa1d8f2fb302004aa6f620042c26b43d110673d2
Alexandre Magrit <alexandre.magrit@student.hers.be>
2026-04-27 21:43:54 +0200

    feat: Première version de la page de login

--- src/main/java/be/hers/pi/comprendre_et_parler/ComprendreEtParlerApplication.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/ComprendreEtParlerApplication.java
@@ -1,23 +1,12 @@
 package be.hers.pi.comprendre_et_parler;
 
-import be.hers.pi.comprendre_et_parler.DAOs.DatabaseConnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ComprendreEtParlerApplication {
 
     public static void main(String[] args) {
-        DatabaseConnector.initialize();
         SpringApplication.run(ComprendreEtParlerApplication.class, args);
     }
-    @GetMapping("/")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("<html><body><h1>Hello %s!</h1></body></html>", name);
-    }
-
 }
--- src/main/java/be/hers/pi/comprendre_et_parler/exceptions/AlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package be.hers.pi.comprendre_et_parler.exceptions;
-
-public class AlreadyExistsException extends RuntimeException {
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
-}
