70acdef799590267ebb825aea7c8f3c93ed5a159
Kerem <kerem.erkart@student.hers.be>
2026-04-28 17:59:26 +0200

    feat : hardcodage des indispo et competences et constructeur de fake interprete

--- src/main/java/be/hers/pi/comprendre_et_parler/controllers/InterpreterController.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/controllers/InterpreterController.java
@@ -1,8 +1,12 @@
 package be.hers.pi.comprendre_et_parler.controllers;
 
+import be.hers.pi.comprendre_et_parler.models.AcademicSkill;
 import be.hers.pi.comprendre_et_parler.models.City;
+import be.hers.pi.comprendre_et_parler.models.ExceptionalUnavailability;
 import be.hers.pi.comprendre_et_parler.models.Interpreter;
+import be.hers.pi.comprendre_et_parler.models.JobSkill;
 import be.hers.pi.comprendre_et_parler.models.Location;
+import be.hers.pi.comprendre_et_parler.models.PunctualTimeSlot;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 
@@ -19,33 +24,8 @@ public class InterpreterController {
 
     @GetMapping
     public String showInterpreterList(Model model) {
-        // temporaire : liste d'un seul faux interprete
         var interpretes = new ArrayList<Interpreter>();
-
-
-        City city = new City("Libramont", 6600);
-        Location location = new Location("Domicile", city, "Rue des Robertos", "12", 0);
-
-        Interpreter fakeInterpreter = new Interpreter(
-                1,
-                "i0001",
-                "Roberto",
-                "Dupont",
-                LocalDate.of(1998, 5, 14),
-                "hashedPassword",
-                "roberto.dupont@test.be",
-                "0470/12.34.56",
-                38,
-                1600,
-                "Tricycle",
-                new HashSet<>(),
-                new HashSet<>(),
-                location,
-                new HashSet<>(),
-                new HashSet<>()
-        );
-
-        interpretes.add(fakeInterpreter);
+        interpretes.add(buildFakeInterpreter(1));
 
         model.addAttribute("interpretes", interpretes);
         return "interpreters/list";
@@ -53,8 +33,23 @@ public class InterpreterController {
 
     @GetMapping("/profile/{id}")
     public String showInterpreterProfile(@PathVariable int id, Model model) {
+        Interpreter fakeInterpreter = buildFakeInterpreter(id);
+
+        model.addAttribute("interprete", fakeInterpreter);
+        return "interpreters/profile";
+    }
+
+    private Interpreter buildFakeInterpreter(int id) {
         City city = new City("Libramont", 6600);
         Location location = new Location("Domicile", city, "Rue des Robertos", "12", 0);
+
+        HashSet<AcademicSkill> academicSkills = new HashSet<>();
+        academicSkills.add(new AcademicSkill("Mathématiques"));
+        academicSkills.add(new AcademicSkill("Informatique"));
+
+        HashSet<JobSkill> jobSkills = new HashSet<>();
+        jobSkills.add(new JobSkill("LSFB"));
+        jobSkills.add(new JobSkill("Interprétation médicale"));
 
         Interpreter fakeInterpreter = new Interpreter(
                 id,
@@ -68,14 +63,43 @@ public class InterpreterController {
                 38,
                 1600,
                 "Tricycle",
-                new HashSet<>(),
-                new HashSet<>(),
+                academicSkills,
+                jobSkills,
                 location,
                 new HashSet<>(),
                 new HashSet<>()
         );
 
-        model.addAttribute("interprete", fakeInterpreter);
-        return "interpreters/profile";
+        HashSet<ExceptionalUnavailability> unavailabilities = new HashSet<>();
+
+        PunctualTimeSlot slot1 = new PunctualTimeSlot(
+                LocalTime.of(9, 0),
+                LocalTime.of(12, 0),
+                LocalDate.of(2026, 5, 2),
+                LocalDate.of(2026, 5, 2)
+        );
+
+        PunctualTimeSlot slot2 = new PunctualTimeSlot(
+                LocalTime.of(14, 0),
+                LocalTime.of(18, 0),
+                LocalDate.of(2026, 5, 5),
+                LocalDate.of(2026, 5, 5)
+        );
+
+        unavailabilities.add(new ExceptionalUnavailability(
+                "Rendez-vous médical",
+                slot1,
+                fakeInterpreter
+        ));
+
+        unavailabilities.add(new ExceptionalUnavailability(
+                "Formation externe",
+                slot2,
+                fakeInterpreter
+        ));
+
+        fakeInterpreter.setUnavailability(unavailabilities);
+
+        return fakeInterpreter;
     }
 }
