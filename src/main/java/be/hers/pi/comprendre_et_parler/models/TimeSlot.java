a39d78e784ebb128e9fdd089611f36f0be30f81e
Christian OME <christian.ome@student.hers.be>
2026-04-28 12:54:51 +0000

    refactor: modifiier les POJOs liés à TimeSlot

--- src/main/java/be/hers/pi/comprendre_et_parler/models/TimeSlot.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/models/TimeSlot.java
@@ -5,39 +5,21 @@ import java.util.Objects;
 
 public abstract class TimeSlot {
     protected int id;
-    protected LocalTime startTime;
-    protected LocalTime endTime;
 
-    public TimeSlot(int id, LocalTime startTime, LocalTime endTime) {
+    public TimeSlot(int id) {
         if (id >= 0) {
             this.id = id;
         } else {
             this.id = 0;
         }
-        this.startTime = startTime;
-        if(startTime.isAfter(this.endTime)) {
-            this.endTime = this.startTime.plusHours(1);
-        }
-        else {
-            this.endTime = endTime;
-        }
     }
 
-    public TimeSlot(LocalTime startTime, LocalTime endTime) {
+    public TimeSlot() {
         this.id = -1;
-        this.startTime = startTime;
-        if(startTime.isAfter(this.endTime)) {
-            this.endTime = this.startTime.plusHours(1);
-        }
-        else {
-            this.endTime = endTime;
-        }
     }
 
     public TimeSlot(TimeSlot timeslot) {
         this.id = timeslot.id;
-        this.startTime = timeslot.startTime;
-        this.endTime = timeslot.endTime;
     }
 
     public int getId() {
@@ -50,44 +32,19 @@ public abstract class TimeSlot {
         }
     }
 
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(LocalTime startTime) {
-        if (startTime.isBefore(this.endTime)) {
-            this.startTime = startTime;
-        }
-    }
-
-    public LocalTime getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(LocalTime endTime) {
-
-        if (endTime.isAfter(this.startTime)) {
-            this.endTime = endTime;
-        }
-    }
-
     /**
      * Checks if the 2 timeslots overlaps partially or totally
      * @param timeSlot 2nd timeslot for the check
      * @return true if the 2 time slots overlaps, or false if it doesn't
      */
-    public boolean overlaps(TimeSlot timeSlot) {
-        return this.startTime.isBefore(timeSlot.endTime) && this.endTime.isAfter(timeSlot.startTime);
-    }
+    public abstract boolean overlaps(TimeSlot timeSlot);
 
     /**
      * Checks if the 2 timeslots overlaps totally
      * @param timeSlot 2nd timeslot for the check
      * @return true if the 2 time slots overlaps totally, or false if it doesn't
      */
-    public boolean overlapsCompletely(TimeSlot timeSlot) {
-        return (this.startTime.isBefore(timeSlot.startTime) && this.endTime.isAfter(timeSlot.endTime)) || (this.startTime.isAfter(timeSlot.startTime) && this.endTime.isBefore(timeSlot.endTime));
-    }
+    public abstract boolean overlapsCompletely(TimeSlot timeSlot);
 
     public abstract TimeSlot clone();
 
@@ -97,7 +54,7 @@ public abstract class TimeSlot {
      */
     @Override
     public String toString() {
-        return "TimeSlot{id=" + this.id + " startTime=" + this.startTime.toString() + " endTime=" + this.endTime.toString() + "}";
+        return "TimeSlot{id=" + this.id + "}";
     }
 
     /**
@@ -106,20 +63,12 @@ public abstract class TimeSlot {
      * @return true if it's the same, else false
      */
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof TimeSlot)) return false;
-
-        TimeSlot other = (TimeSlot) o;
-        return this.startTime.equals(other.startTime) && this.endTime.equals(other.endTime);
-    }
+    public abstract boolean equals(Object o);
 
     /**
      * Return the hashcode of TimeSlot
      * @return an integer whith is the hashcode of TimeSlot
      */
     @Override
-    public int hashCode() {
-        return Objects.hash(this.startTime, this.endTime);
-    }
+    public abstract int hashCode();
 }
