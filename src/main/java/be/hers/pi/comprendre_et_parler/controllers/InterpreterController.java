286078efe1a2994cedffe1754dddc1a99a2e0443
Kerem <kerem.erkart@student.hers.be>
2026-04-29 11:07:34 +0200

    feat : ajout d'un builder d'interprete FAKE pour tester (fonction temporaire) dans le controlleur, ajout de la gestion de page pour la liste d'interprete, ajout bouton en bootstrap pour naviguer

--- src/main/java/be/hers/pi/comprendre_et_parler/controllers/InterpreterController.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/controllers/InterpreterController.java
@@ -12,22 +12,58 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 @Controller
 @RequestMapping("/interpreters")
 public class InterpreterController {
 
     @GetMapping
-    public String showInterpreterList(Model model) {
-        var interpretes = new ArrayList<Interpreter>();
-        interpretes.add(buildFakeInterpreter(1));
+    public String showInterpreterList(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "") String keyword, Model model) {
 
-        model.addAttribute("interpretes", interpretes);
+        int interpretersPerPage = 10;
+
+        List<Interpreter> allInterpreters = buildFakeInterpreters();
+        List<Interpreter> filteredInterpreters = filterInterpreters(allInterpreters, keyword);
+        int totalInterpreters = filteredInterpreters.size();
+
+        int totalPages = calculateTotalPages(totalInterpreters, interpretersPerPage);
+
+        if (page < 1) {
+            page = 1;
+        }
+
+        if (page > totalPages) {
+            page = totalPages;
+        }
+
+        List<Interpreter> interpretersForCurrentPage = getInterpretersForPage(filteredInterpreters, page, interpretersPerPage);
+
+        int startItem = 0;
+        int endItem = 0;
+
+        if (totalInterpreters > 0) {
+            startItem = (page - 1) * interpretersPerPage + 1;
+            endItem = startItem + interpretersForCurrentPage.size() - 1;
+        }
+
+        model.addAttribute("interpretes", interpretersForCurrentPage);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("pageNumber", page);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("totalItems", totalInterpreters);
+        model.addAttribute("startItem", startItem);
+        model.addAttribute("endItem", endItem);
+        model.addAttribute("hasPrevious", page > 1);
+        model.addAttribute("hasNext", page < totalPages);
+        model.addAttribute("currentPage", "interpreters");
+
         return "interpreters/list";
     }
 
@@ -36,10 +72,95 @@ public class InterpreterController {
         Interpreter fakeInterpreter = buildFakeInterpreter(id);
 
         model.addAttribute("interprete", fakeInterpreter);
+        model.addAttribute("currentPage", "interpreters");
+
         return "interpreters/profile";
     }
 
+    private List<Interpreter> filterInterpreters(List<Interpreter> interpreters, String keyword) {
+        List<Interpreter> filteredInterpreters = new ArrayList<>();
+
+        String searchedText = keyword.trim().toLowerCase();
+
+        for (Interpreter interpreter : interpreters) {
+            String login = interpreter.getLogin().toLowerCase();
+            String firstName = interpreter.getFirstName().toLowerCase();
+            String lastName = interpreter.getLastName().toLowerCase();
+
+            boolean matchesLogin = login.contains(searchedText);
+            boolean matchesFirstName = firstName.contains(searchedText);
+            boolean matchesLastName = lastName.contains(searchedText);
+
+            if (searchedText.isEmpty() || matchesLogin || matchesFirstName || matchesLastName) {
+                filteredInterpreters.add(interpreter);
+            }
+        }
+
+        return filteredInterpreters;
+    }
+
+    private int calculateTotalPages(int totalItems, int itemsPerPage) {
+        if (totalItems == 0) {
+            return 1;
+        }
+
+        int totalPages = totalItems/ itemsPerPage;
+
+        if (totalItems %itemsPerPage  != 0) {
+            totalPages = totalPages + 1;
+        }
+
+        return totalPages;
+    }
+
+    private List<Interpreter> getInterpretersForPage(List<Interpreter> interpreters, int page, int itemsPerPage) {
+        List<Interpreter> interpretersForPage = new ArrayList<>();
+
+        int startIndex = (page - 1) * itemsPerPage;
+        int endIndex = startIndex + itemsPerPage;
+
+        if (endIndex > interpreters.size()) {
+            endIndex = interpreters.size();
+        }
+
+        for (int i = startIndex; i < endIndex; i++) {
+            interpretersForPage.add(interpreters.get(i));
+        }
+
+        return interpretersForPage;
+    }
+
+    //FONCTION TEMPORAIRE
+    private List<Interpreter> buildFakeInterpreters() {
+        List<Interpreter> interpreters = new ArrayList<>();
+
+        interpreters.add(buildFakeInterpreter(1, "Roberto", "Dupont"));
+        interpreters.add(buildFakeInterpreter(2, "Julie", "Leroy"));
+        interpreters.add(buildFakeInterpreter(3, "Amine", "Bernard"));
+        interpreters.add(buildFakeInterpreter(4, "Sarah", "Simon"));
+        interpreters.add(buildFakeInterpreter(5, "Nicolas", "Legrand"));
+        interpreters.add(buildFakeInterpreter(6, "Emma", "Lambert"));
+        interpreters.add(buildFakeInterpreter(7, "Louis", "Fontaine"));
+        interpreters.add(buildFakeInterpreter(8, "Ines", "Leclercq"));
+        interpreters.add(buildFakeInterpreter(9, "Thomas", "Remy"));
+        interpreters.add(buildFakeInterpreter(10, "Lina", "Petit"));
+        interpreters.add(buildFakeInterpreter(11, "Hugo", "Marchal"));
+        interpreters.add(buildFakeInterpreter(12, "Nora", "Colin"));
+        interpreters.add(buildFakeInterpreter(13, "Lucas", "Hubert"));
+        interpreters.add(buildFakeInterpreter(14, "Jade", "Henry"));
+        interpreters.add(buildFakeInterpreter(15, "Noah", "Mertens"));
+
+        return interpreters;
+    }
+
+    //FONCTION TEMPORAIRE
+
     private Interpreter buildFakeInterpreter(int id) {
+        return buildFakeInterpreter(id, "Roberto", "Dupont");
+    }
+
+    //FONCTION TEMPORAIRE
+    private Interpreter buildFakeInterpreter(int id, String firstName, String lastName) {
         City city = new City("Libramont", 6600);
         Location location = new Location("Domicile", city, "Rue des Robertos", "12", 0);
 
@@ -53,12 +174,12 @@ public class InterpreterController {
 
         Interpreter fakeInterpreter = new Interpreter(
                 id,
-                "i0001",
-                "Roberto",
-                "Dupont",
+                "i" + String.format("%04d", id),
+                firstName,
+                lastName,
                 LocalDate.of(1998, 5, 14),
                 "hashedPassword",
-                "roberto.dupont@test.be",
+                (firstName + "." + lastName + "@test.be").toLowerCase(),
                 "0470/12.34.56",
                 38,
                 1600,
@@ -82,17 +203,20 @@ public class InterpreterController {
                 LocalDateTime.of(2026, 5, 9, 0, 0)
         );
 
-        unavailabilities.add(new ExceptionalUnavailability(
+        ExceptionalUnavailability unavailability1 = new ExceptionalUnavailability(
                 "Rendez-vous médical",
                 slot1,
                 fakeInterpreter
-        ));
+        );
 
-        unavailabilities.add(new ExceptionalUnavailability(
+        ExceptionalUnavailability unavailability2 = new ExceptionalUnavailability(
                 "Congé maladie",
                 slot2,
                 fakeInterpreter
-        ));
+        );
+
+        unavailabilities.add(unavailability1);
+        unavailabilities.add(unavailability2);
 
         fakeInterpreter.setUnavailability(unavailabilities);
 
