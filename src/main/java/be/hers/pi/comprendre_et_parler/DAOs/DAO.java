0bc79b09c15044d8a0b5a6bdac2e36133fe8dd4c
Luna Lorenz <luna.lorenz@student.hers.be>
2026-04-29 15:06:08 +0200

    feat: transaction wrapper for FunctionWithSQLException taking one parameter

--- src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAO.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAO.java
@@ -1,76 +1,84 @@
 package be.hers.pi.comprendre_et_parler.DAOs;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.NoSuchElementException;
 
 import be.hers.pi.comprendre_et_parler.exceptions.AlreadyExistsException;
+import be.hers.pi.comprendre_et_parler.exceptions.ConnectionException;
 
+import javax.xml.crypto.Data;
 import java.util.Set;
+
 
 public abstract class DAO<T> {
 
-    public static <T, R> R call(FunctionWithSQLException<T, R> func, T param, boolean noSavePoint) {
-        R ret = null;
+    /**
+     * Wraps a DAO method in an SQL transaction and single out connection exceptions
+     * @param daoMethod the method to call
+     * @param param the parameter to pass to the method
+     * @param useTransaction specifies whether to run the method within an SQL transaction (default: True)
+     * @return the method's return value
+     * @param <T> the method's input parameter type
+     * @param <R> the method's return type
+     * @throws ConnectionException if a connection error occurred (SQL state 08xxx)
+     * @throws SQLException if any other error occurred
+     */
+    public static <T, R> R call(FunctionWithSQLException<T, R> daoMethod, T param, boolean useTransaction) throws SQLException, ConnectionException {
         try {
-            // Begin transaction
-            ret = func.apply(param);
-            // Commit
+            if (useTransaction) {
+                DatabaseConnector.getInstance().setAutoCommit(false);
+                Savepoint sp = DatabaseConnector.getInstance().setSavepoint();
+                try {
+                    R ret = daoMethod.apply(param);
+                    DatabaseConnector.getInstance().commit();
+                    return ret;
+                }
+                catch (Exception e) {
+                    DatabaseConnector.getInstance().rollback(sp);
+                    throw e;
+                }
+                finally {
+                    DatabaseConnector.getInstance().setAutoCommit(true);
+                    try {
+                        DatabaseConnector.getInstance().releaseSavepoint(sp);
+                    }
+                    catch (SQLFeatureNotSupportedException e) {
+                        System.err.println("Warning: " + e.getMessage());
+                    }
+                }
+            }
+            else {
+                return daoMethod.apply(param);
+            }
         }
         catch (SQLException e) {
-            e.printStackTrace();
-            // Handle reconnection
+            if (e.getSQLState().matches("^08")) {
+                throw new ConnectionException("Could not connect to database");
+            }
+            throw e;
         }
-        return ret;
     }
-    public static <T, R> R call(FunctionWithSQLException<T, R> func, T param) {
-        return call(func, param, false);
+
+    public static <T, R> R call(FunctionWithSQLException<T, R> func, T param) throws SQLException {
+        return call(func, param, true);
     }
 
     public static <T, U, R> R call(BiFunctionWithSQLException<T, U, R> func, T param1, U param2, boolean noSavePoint) {
-        R ret = null;
-        try {
-            // Begin transaction
-            ret = func.apply(param1, param2);
-            // Commit
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-            // Handle reconnection
-        }
-        return ret;
+        throw new UnsupportedOperationException();
     }
     public static <T, U, R> R call(BiFunctionWithSQLException<T, U, R> func, T param1, U param2) {
         return call(func, param1, param2, false);
     }
 
     public static <T> void call(ConsumerWithSQLException<T> cons, T param, boolean noSavePoint) {
-        try {
-            // Begin transaction
-            cons.apply(param);
-            // Commit
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-            // Handle reconnection
-        }
+        throw new UnsupportedOperationException();
     }
     public static <T, U> void call(ConsumerWithSQLException<T> cons, T param1, U param2) {
         call(cons, param1, false);
     }
 
     public static <T, U> void call(BiConsumerWithSQLException<T, U> cons, T param1, U param2, boolean noSavePoint) {
-        try {
-            // Begin transaction
-            cons.apply(param1, param2);
-            // Commit
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-            // Handle reconnection
-        }
+        throw new UnsupportedOperationException();
     }
     public static <P> void call(ConsumerWithSQLException<P> cons, P param) {
         call(cons, param, false);
@@ -81,15 +89,17 @@ public abstract class DAO<T> {
      * @return the object identified by id in database, or null if none was present
      * @throws SQLException if the database could not be reached
      */
-    abstract T find(int id) throws SQLException;
+    public abstract T find(int id) throws SQLException;
 
     /**
      * @param objectToInsert an object of type T to add to the database
      * @post objectToInsert has been added to the database, and the change was commited
-     * @throws AlreadyExistsException if objectToInsert is already present in database
+     * @throws AlreadyExistsException if an object with a different id but otherwise identical fields already exists in database
      * @throws SQLException if the insertion failed for any other reason
+     * @post objectToInsert has been added to the database, the object is updated with auto generated id from the database,
+     * and the change was commited
      */
-    abstract void create(T objectToInsert) throws AlreadyExistsException, SQLException;
+    public abstract void create(T objectToInsert) throws AlreadyExistsException, SQLException;
 
     /**
      * @param objectToUpdate the object to edit in the database
@@ -98,22 +108,21 @@ public abstract class DAO<T> {
      * @throws AlreadyExistsException if an object with a different id but otherwise identical fields already exists in database
      * @throws SQLException if the update failed for any other reason
      */
-    abstract void update(T objectToUpdate) throws NoSuchElementException, AlreadyExistsException, SQLException;
+    public abstract void update(T objectToUpdate) throws NoSuchElementException, AlreadyExistsException, SQLException;
 
     /**
-     *
      * @param objectToDelete the object to delete in the database
      * @post the object matching every attribute of objectToDelete has been deleted from the database, and the change was commited
      * @throws NoSuchElementException if no object matching every attribute of objectToDelete was present in the database
      * @throws SQLException if the deletion failed for any other reason
      */
-    abstract void delete(T objectToDelete) throws NoSuchElementException, SQLException;
+    public abstract void delete(T objectToDelete) throws NoSuchElementException, SQLException;
 
     /**
      * @return every object of the corresponding type present in database (possibly an empty list)
      * @throws SQLException if the database could not be reached
      */
-    abstract Set<T> findAll() throws SQLException;
+    public abstract Set<T> findAll() throws SQLException;
 
     /**
      * Check if an object already exists in the database
@@ -122,7 +131,7 @@ public abstract class DAO<T> {
      * @throws SQLException if the database could not be reached
      */
     protected abstract boolean checkAlreadyExists(T object) throws SQLException;
-    
+
     /**
      * Build an object from a ResultSet
      * @param result the ResultSet to read from
