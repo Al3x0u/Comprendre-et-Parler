4f5c7241e8a812f4a7e64afbb576ace1a4042f7a
ryandeom <ryan.deom@student.hers.be>
2026-05-03 22:25:46 +0200

    Feat : Création du fichier MissionService.java + Ajout de la méthode pour chercher toutes les missions d'une semaine dans DAOMission

--- src/main/java/be/hers/pi/comprendre_et_parler/services/MissionService.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/services/MissionService.java
@@ -11,28 +11,10 @@ import java.util.ArrayList;
 import java.util.Set;
 
 public class MissionService {
-    /**
-     * Return the list of missions for a given week, filtered according to the user's role.
-     * @param user the user requesting the schedule (Manager, Interpreter or Beneficiary)
-     * @param weekStart the date of any day within the target week;
-     * @throws SQLException if the database could not be reached
-     */
     public ArrayList<Mission> getMissionsForWeek(AppliUser user, LocalDate weekStart) throws SQLException {
 
         DAOMission daoMission = new DAOMission();
 
-        int yearNumber = weekStart.getYear();
-        int weekNumber = weekStart.get(java.time.temporal.WeekFields.ISO.weekOfWeekBasedYear());
-
-        Set<Mission> missions;
-
-        if (user instanceof Manager) {
-            missions = daoMission.getAllMissionsForWeek(yearNumber, weekNumber);
-        }
-        else {
-            missions = daoMission.getScheduleForWeek(user.getId(), yearNumber, weekNumber);
-        }
-
-        return new ArrayList<>(missions);
+        
     }
 }
