f9c4b233e93e60a069a2d7f8e57c08ea6aa7811b
ryandeom <ryan.deom@student.hers.be>
2026-05-05 22:13:47 +0200

    Feat(MissionFilter) : Implémentation de la classe qui servira à filter les missions

---
+++ src/main/java/be/hers/pi/comprendre_et_parler/models/MissionFilter.java
@@ -0,0 +1,10 @@
+package be.hers.pi.comprendre_et_parler.models;
+
+public class MissionFilter {
+    private Beneficiary beneficiary;
+    private Interpreter interpreter;
+    private JobSkill jobSkill;
+    private AcademicSkill academicSkill;
+    private Location location;
+    private Integer minImportance;
+}
