4a63e3c21c5b50b352e2df2bc745aef345a5db23
ryandeom <ryan.deom@student.hers.be>
2026-05-05 22:20:39 +0200

    feat: ajout des getters et setters du filtre

--- src/main/java/be/hers/pi/comprendre_et_parler/models/MissionFilter.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/models/MissionFilter.java
@@ -7,4 +7,52 @@ public class MissionFilter {
     private AcademicSkill academicSkill;
     private Location location;
     private Integer minImportance;
+
+    public Beneficiary getBeneficiary(){
+        return beneficiary;
+    }
+
+    public Interpreter getInterpreter(){
+        return interpreter;
+    }
+
+    public JobSkill getJobSkill(){
+        return jobSkill;
+    }
+
+    public AcademicSkill getAcademicSkill(){
+        return academicSkill;
+    }
+
+    public Location getLocation(){
+        return location;
+    }
+
+    public Integer getMinImportance(){
+        return minImportance;
+    }
+
+    public void setBeneficiary(Beneficiary beneficiary){
+        this.beneficiary = beneficiary;
+    }
+
+    public void setInterpreter(Interpreter interpreter){
+        this.interpreter = interpreter;
+    }
+
+    public void setJobSkill(JobSkill jobSkill){
+        this.jobSkill = jobSkill;
+    }
+
+    public void setAcademicSkill(AcademicSkill academicSkill){
+        this.academicSkill = academicSkill;
+    }
+
+    public void setLocation(Location location){
+        this.location = location;
+    }
+
+    public void setMinImportance(Integer minImportance){
+        this.minImportance = minImportance;
+    }
 }
