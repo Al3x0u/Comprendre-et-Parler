b0fad85a4b19f2a17011c436dd8e132017458e8e
matelot-pret <pj.samou@student.hers.be>
2026-05-07 21:51:34 +0200

    refactor : ajout des classes de controllers et modification du fichier pom.xml

---
+++ src/main/java/be/hers/pi/comprendre_et_parler/services/LoginService.java
@@ -0,0 +1,47 @@
+package be.hers.pi.comprendre_et_parler.services;
+
+import be.hers.pi.comprendre_et_parler.DAOs.*;
+import be.hers.pi.comprendre_et_parler.models.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+
+@Service
+public class LoginService {
+
+    public AppliUser getUserData(String login, String password){
+        AppliUser user = null;
+        if(login.trim().startsWith("r")){
+            try {
+                user = new DAOManager().find(login);
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }else if(login.trim().startsWith("b")){
+            try {
+                user = new DAOBeneficiary().find(login);
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }else if(login.trim().startsWith("i")){
+            try {
+                user = new DAOInterpreter().find(login);
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }else{
+            return null;
+        }
+        if(user == null || !checkUserLogin(user, login, password) ) {
+            return null;
+        }
+
+        return user;
+    }
+
+    private boolean checkUserLogin(AppliUser user , String login, String password){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return user.getLogin().equals(login) && encoder.matches(password, user.getHashedPassword());
+    }
+}
