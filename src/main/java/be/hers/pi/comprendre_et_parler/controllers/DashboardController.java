2d3ac17bb3a07ebbb7699547ee3b40d0a795b4de
matelot-pret <pj.samou@student.hers.be>
2026-05-11 08:08:07 +0200

    merge : ajout de verification du mot de passe

--- src/main/java/be/hers/pi/comprendre_et_parler/controllers/DashboardController.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/controllers/DashboardController.java
@@ -16,7 +16,7 @@ public class DashboardController {
     @ResponseBody
     public String hash(){
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        return encoder.encode("Inter02PI");
+        return encoder.encode("Resp01PI");
     }
 
     /**
