f63cd5e277ba0b75b15415b8bb5f4094ee565f67
Kerem <kerem.erkart@student.hers.be>
2026-05-16 19:18:35 +0200

    feat : ajout des pages sur la méthode addInterceptor du webconfig

--- src/main/java/be/hers/pi/comprendre_et_parler/controllers/interceptors/WebConfig.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/controllers/interceptors/WebConfig.java
@@ -19,9 +19,44 @@ public class WebConfig implements WebMvcConfigurer {
      * except those matching the excluded path patterns
      */
     @Override
-    public void addInterceptors(InterceptorRegistry registry){
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(sessionInterceptor)
-                .excludePathPatterns("/login", "/", "/hash", "/css/**", "/js/**", "/images/**");
+                .addPathPatterns(
+                        "/dashboard",
+                        "/horaire",
+                        "/profil",
+                        "/profil/modifier-mot-de-passe",
+                        "/gestion",
+                        "/gestion/competences/academiques/ajouter",
+                        "/gestion/competences/academiques/*/modifier",
+                        "/gestion/competences/academiques/*/supprimer",
+                        "/gestion/competences/metier/ajouter",
+                        "/gestion/competences/metier/*/modifier",
+                        "/gestion/competences/metier/*/supprimer",
+                        "/gestion/statuts/ajouter",
+                        "/gestion/statuts/*/modifier",
+                        "/gestion/statuts/*/supprimer",
+                        "/interpretes",
+                        "/interpretes/profil/*",
+                        "/interpretes/profil/*/modifier",
+                        "/interpretes/profil/*/promouvoir",
+                        "/interpretes/creation",
+                        "/beneficiaires",
+                        "/beneficiaires/profil/*/modifier",
+                        "/beneficiaires/creer"
+                )
+                .excludePathPatterns(
+                        "/",
+                        "/login",
+                        "/logout",
+                        "/hash",
+                        "/error",
+                        "/css/**",
+                        "/js/**",
+                        "/images/**",
+                        "/webjars/**",
+                        "/favicon.ico"
+                );
     }
 }
 
