e05be9c0381d3d79f1af11687ef656463a61a010
Allan <aandre@student.hers.be>
2026-06-01 16:39:23 +0200

    fix(Beneficiary) : comparer le login de l'interprète de référence dans le equals

--- src/main/java/be/hers/pi/comprendre_et_parler/models/Beneficiary.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/models/Beneficiary.java
@@ -111,7 +111,7 @@ public class Beneficiary extends AppliUser {
 
         Beneficiary other = (Beneficiary) o;
         return (super.equals(other) && Objects.equals(status, other.status)
-                && Objects.equals(interpreterRef, other.interpreterRef));
+                && Objects.equals(interpreterRef.getLogin(), other.interpreterRef.getLogin()));
     }
 
     /**
@@ -124,7 +124,7 @@ public class Beneficiary extends AppliUser {
         return Objects.hash(
                 super.hashCode(),
                 status,
-                interpreterRef
+                interpreterRef.getLogin()
         );
     }
 
