673f6d9deca2f46113adbd1fff4962a51b0ec095
Allan <aandre@student.hers.be>
2026-06-15 18:27:50 +0200

    feat(database) : accepter les demandes en attentes

--- database/Script_initialisation_BD.sql
+++ database/Script_initialisation_BD.sql
@@ -105,7 +105,7 @@ INSERT INTO TimeSlot VALUES(NULL, to_date('2026/06/20 10:00', 'yyyy/mm/dd hh24:m
 INSERT INTO Mission VALUES(NULL, 5, 0, 'Cours de chimie', 1, NULL, 1, 1, 6, 9, 'B9');
 INSERT INTO Mission VALUES(NULL, 5, 1, 'Cours de biologie', 1, NULL, 2, 3, 5, 9, 'B9');
 INSERT INTO Mission VALUES(NULL, 5, 0, 'examen médical', 1, NULL, 1, 3, 2, 7, NULL);
-INSERT INTO Mission VALUES(NULL, 6, 2, 'Cours de néérlandais', 0, NULL, 3, 1, 3, 4, 'A3');
+INSERT INTO Mission VALUES(NULL, 6, 2, 'Cours de néérlandais', 1, NULL, 3, 1, 3, 4, 'A3');
 INSERT INTO Mission VALUES(NULL, 5, 0, 'Cours de chimie', 2, NULL, 4, 2, 6, 9, 'B8');
 INSERT INTO Mission VALUES(NULL, 5, 3, 'sortie scolaire', 5, 'Visite de Bruxelles', 5, 1, 1, 9, NULL);
 
