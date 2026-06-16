85bfcf6038d0c0cd930fcf6f2019181c2db3f4d2
Allan <aandre@student.hers.be>
2026-06-16 20:36:03 +0200

    refactor(DAOInterpreter) : faire en sorte que findAvailable ne prennent pas les interprètes ayant des mission en cours

--- src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAOInterpreter.java
+++ src/main/java/be/hers/pi/comprendre_et_parler/DAOs/DAOInterpreter.java
@@ -738,7 +738,7 @@ public class DAOInterpreter extends DAO<Interpreter> {
                         " LEFT JOIN " + DAOPunctualTimeSlot.TABLE + " tm ON m." + DAOMission.FIELD_TIME_SLOT + " = tm." + DAOPunctualTimeSlot.FIELD_ID +
                         " LEFT JOIN " + DAOExceptionalUnavailability.TABLE + " u ON i." + FIELD_ID + " = u." + DAOExceptionalUnavailability.FIELD_ID_INTERPRETER +
                         " LEFT JOIN " + DAOPunctualTimeSlot.TABLE + " tu ON u." + DAOExceptionalUnavailability.FIELD_ID_TIMESLOT + " = tu." + DAOPunctualTimeSlot.FIELD_ID +
-                        " WHERE (tm." + DAOPunctualTimeSlot.FIELD_START_TIME + " <= ? AND tm." + DAOPunctualTimeSlot.FIELD_END_TIME + " >= ?)" +
+                        " WHERE (tm." + DAOPunctualTimeSlot.FIELD_START_TIME + " <= ? AND tm." + DAOPunctualTimeSlot.FIELD_END_TIME + " >= ?  AND m." + DAOMission.FIELD_STATE + " = ?))" +
                         " OR (tu." + DAOPunctualTimeSlot.FIELD_START_TIME + " <= ? AND tu." + DAOPunctualTimeSlot.FIELD_END_TIME + " >= ?))"
         );
         Set<Interpreter> interpreters = new HashSet<>();
@@ -746,10 +746,11 @@ public class DAOInterpreter extends DAO<Interpreter> {
         ResultSet result = null;
         try {
             statement = DatabaseConnector.getInstance().prepareStatement(query);
-            statement.setTimestamp(1, Timestamp.valueOf(start));
-            statement.setTimestamp(2, Timestamp.valueOf(end));
-            statement.setTimestamp(3, Timestamp.valueOf(start));
+            statement.setTimestamp(1, Timestamp.valueOf(end));
+            statement.setTimestamp(2, Timestamp.valueOf(start));
+            statement.setInt(3, MissionState.ACCEPTED.getValue());
             statement.setTimestamp(4, Timestamp.valueOf(end));
+            statement.setTimestamp(5, Timestamp.valueOf(start));
 
             result = statement.executeQuery();
             while (result.next())
